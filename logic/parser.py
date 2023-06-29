9d8424e7fdcc38ce1ac007ecbf92bfc38517824a
oytarhanova <oytarhanova@fintechiq.ru>
2023-06-29 22:10:42 +0700

    update parsing

--- logic/parser.py
+++ logic/parser.py
@@ -1,6 +1,6 @@
 import re
 from datetime import datetime
-from logic.rules import DATE_RULE, SCHEDULE_RULE, COMMENTS_RULE
+from logic.rules import DATE_RULE, SCHEDULE_RULE, COMMENTS_RULE, TRAIN_RESULTS_RULE
 from persistance.models import TaskModel
 
 DAYS = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье']
@@ -13,6 +13,25 @@ class Parser:
         if schedule:
             return schedule.group(2)
 
+    def get_train_results(self, text):
+        train_results = TRAIN_RESULTS_RULE.search(text)
+        if train_results:
+            return train_results.group(2)
+
+    def get_comments(self, text):
+        comments = COMMENTS_RULE.search(text)
+        if comments:
+            return comments.group()
+
+    def get_week(self, msg_date):
+        week = msg_date.isocalendar()[1]
+        return week
+
+    def get_year(self, msg_date):
+        year = msg_date.year
+        return year
+
+
     def parse_tasks(self, schedule, msg_date):
         tasks = []
         if not schedule:
@@ -22,21 +41,18 @@ class Parser:
                 info = re.search(rf'({DATE_RULE})(, {DAYS[i]})((.|\n)+)({DATE_RULE}, {DAYS[i + 1]})', schedule)
             else:
                 info = re.search(rf'({DATE_RULE})(, {DAYS[i]})((.|\n)+)', schedule)
-            task = info.group(5)
-            day_of_week = DAYS[i]
-            week = msg_date.isocalendar()[1]
-            date = self.postprocess_date(info.group(2, 3), msg_date)
-            new_task = TaskModel(date=date, day_of_week=day_of_week, week=week,
-                                 task=task)
-            tasks.append(new_task)
+            if info:
+                task = info.group(5)
+                day_of_week = DAYS[i]
+                week = msg_date.isocalendar()[1]
+                date = self.postprocess_date(info.group(2, 3), msg_date)
+                new_task = TaskModel(date=date, day_of_week=day_of_week, week=week, task=task)
+                tasks.append(new_task)
         return tasks
 
-    def get_comments(self, text):
-        return COMMENTS_RULE.search(text).group()
-
-    def postprocess_date(self, date, msg_date):
-        day = int(date[0])
-        month_name = date[1]
+    def postprocess_date(self, task_date, msg_date):
+        day = int(task_date[0])
+        month_name = task_date[1]
 
         month = {
             "января": 1,
