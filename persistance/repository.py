d0f9a8a81f6b89413306bf240165dad37bd9787d
oytarhanova <oytarhanova@fintechiq.ru>
2023-07-12 13:38:41 +0700

    fix filtering

--- persistance/repository.py
+++ persistance/repository.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date
 from sqlalchemy import select
 from persistance.postgres_connection import PostgresConnector
 from persistance.models import ScheduleModel, TaskModel
@@ -18,24 +18,26 @@ class Repository:
         self.postgres_session.add(model)
         self.postgres_session.commit()
 
-    async def get_schedule_by_date(self, date: datetime):
-        stmt = select(ScheduleModel.schedule).filter(
-            ScheduleModel.week == date.isocalendar().week and ScheduleModel.year == date.year
-        )
+    async def get_schedule_by_date(self, selected_date: date):
+        stmt = select(ScheduleModel.schedule)\
+            .filter(ScheduleModel.week == selected_date.isocalendar().week)\
+            .filter(ScheduleModel.year == selected_date.year)
         return self.postgres_session.scalar(stmt)
 
-    async def get_task_by_date(self, date: datetime):
-        stmt = select(TaskModel.task).filter(TaskModel.date == date)
+    async def get_task_by_date(self, selected_date: date):
+        stmt = select(TaskModel.task).filter(TaskModel.date == selected_date)
         return self.postgres_session.scalar(stmt)
 
-    async def get_comment_by_date(self, date: datetime):
-        stmt = select(ScheduleModel.comment).filter(
-            ScheduleModel.week == date.isocalendar().week and ScheduleModel.year == date.year
-        )
+    async def get_comment_by_date(self, selected_date: date):
+        stmt = select(ScheduleModel.comment)\
+            .filter(ScheduleModel.week == selected_date.isocalendar().week)\
+            .filter(ScheduleModel.year == selected_date.year)
         return self.postgres_session.scalar(stmt)
 
-    async def get_train_result_by_date(self, date: datetime):
-        stmt = select(ScheduleModel.comment).filter(
-            ScheduleModel.week == date.isocalendar().week and ScheduleModel.year == date.year
-        )
+    async def get_train_result_by_date(self, selected_date: date):
+        week = selected_date.isocalendar().week
+        year = selected_date.year if week >= 1 else selected_date.year - 1
+        stmt = select(ScheduleModel.train_results)\
+            .filter(ScheduleModel.week == week)\
+            .filter(ScheduleModel.year == year)
         return self.postgres_session.scalar(stmt)
